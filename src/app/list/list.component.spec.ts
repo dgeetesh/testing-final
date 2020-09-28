fc0969a1b191674f99329da943502979f1cdeb31
Keshav Vishwkarma <dgeetesh@gmail.com>
2020-09-28 21:12:45 +0530

    first commit

---
+++ src/app/list/list.component.spec.ts
@@ -0,0 +1,39 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ListComponent } from './list.component';
+
+describe('ListComponent', () => {
+  let component: ListComponent;
+  let fixture: ComponentFixture<ListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(ListComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+
+  it('should create', () => {
+    let fixture = TestBed.createComponent(ListComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('User Array length should be 0 before detect change', () => {
+    let fixture = TestBed.createComponent(ListComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.userList.length).toEqual(0);
+  });
+
+  it('User Array length should not be 0', () => {
+    let fixture = TestBed.createComponent(ListComponent);
+    let app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    expect(app.userList.length).toEqual(2);
+  });
+});
